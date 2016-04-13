71d8116b9f97c13bec3fd42fbe06bd76f3810fac
Amanda Lin <adnama.lin@gmail.com>
2016-04-13 14:05:59 -0500

    Gather vampire information

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "New Employee Data: Fill Out Truthfully"
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
+garlic_test = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance_test = gets.chomp
