766e96f398df60c433d6ff870cbef12d8764ec7f
Amanda Lin <adnama.lin@gmail.com>
2016-04-14 00:29:02 -0500

    Add allergies test

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,43 +1,50 @@
 
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be taking the test this session?"
+number_employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+until number_employees == 0
+  puts "What is your name?"
+  name = gets.chomp.downcase
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
-garlic = gets.chomp.upcase
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp.upcase
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
+  garlic = gets.chomp.upcase
 
-#variables set to true when the input answer indicates non-vampire traits
-case
-  when age == 2016 - birth_year
-  age_test = true
-  when garlic == "Y"
-  garlic_test = true
-  when insurance == "Y"
-  insurance_test = true
-  when name != "Drake Cula" && name != "Tu Fang"
-  name_test = true
-end
+  puts "Would you like to enroll in the company’s health insurance?"
+  insurance = gets.chomp.upcase
 
-if !name_test && (age_test && garlic_test &&    insurance_test)
-  puts "definitely a vampire."
+    if (name == "drake cula" || name == "tu fang")
+        vampire_status = "definitely a vampire."
+      elsif (age == 2016 - birth_year) && (garlic == "Y" || insurance == "Y")
+        vampire_status = "probably not a vampire"
+      elsif (age != 2016 - birth_year) && garlic == "N" && insurance == "N"
+        vampire_status = "almost certainly a vampire"
+      elsif (age != 2016 - birth_year) && (garlic == "N" || insurance == "N")
+        vampire_status = "probably a vampire"
+      else
+        vampire_status = "inconclusive"
+    end
 
-  elsif age_test && (garlic_test || insurance_test)
-  puts "probably not a vampire"
+allergies = ""
+  until allergies == "done" || allergies == "sunshine"
+    puts "Please type out your allergies. If none or you have finished please type done"
+    allergies = gets.chomp
+      if allergies == "sunshine"
+        vampire_status = "probabaly a vampire"
+      elsif allergies == "done"
+        puts "Thanks for your submission"
+      else
+        puts "#{allergies} has been added. Type 'done' when finished"
+      end
+  end
 
-  elsif !age_test && !garlic_test && !insurance_test
-  puts "almost certainly a vampire"
+number_employees -= 1
+	puts "Test Results for #{name} is #{vampire_status}"
+    puts "Ready for next employee."
 
-  elsif !age_test && (!garlic_test || !insurance_test)
-  puts "probably a vampire"
-
-  else
-  puts "Results inconclusive"
 end
