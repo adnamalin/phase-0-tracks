6042f6ef16600c3f4ec67b18aa14daaaf6178377
Amanda Lin <adnama.lin@gmail.com>
2016-04-14 00:37:09 -0500

    Add plot twist

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,3 @@
-
 puts "How many employees will be taking the test this session?"
 number_employees = gets.chomp.to_i
 
@@ -18,33 +17,38 @@ until number_employees == 0
   puts "Would you like to enroll in the company’s health insurance?"
   insurance = gets.chomp.upcase
 
-    if (name == "drake cula" || name == "tu fang")
-        vampire_status = "definitely a vampire."
-      elsif (age == 2016 - birth_year) && (garlic == "Y" || insurance == "Y")
-        vampire_status = "probably not a vampire"
-      elsif (age != 2016 - birth_year) && garlic == "N" && insurance == "N"
-        vampire_status = "almost certainly a vampire"
-      elsif (age != 2016 - birth_year) && (garlic == "N" || insurance == "N")
-        vampire_status = "probably a vampire"
-      else
-        vampire_status = "inconclusive"
-    end
+        if (name == "drake cula" || name == "tu fang")
+            vampire_status = "definitely a vampire."
+          elsif (age == 2016 - birth_year) && (garlic == "Y" || insurance == "Y")
+            vampire_status = "probably not a vampire"
+          elsif (age != 2016 - birth_year) && garlic == "N" && insurance == "N"
+            vampire_status = "almost certainly a vampire"
+          elsif (age != 2016 - birth_year) && (garlic == "N" || insurance == "N")
+            vampire_status = "probably a vampire"
+          else
+            vampire_status = "inconclusive"
+        end
 
-allergies = ""
+  allergies = ""
   until allergies == "done" || allergies == "sunshine"
-    puts "Please type out your allergies. If none or you have finished please type done"
-    allergies = gets.chomp
-      if allergies == "sunshine"
-        vampire_status = "probabaly a vampire"
-      elsif allergies == "done"
-        puts "Thanks for your submission"
-      else
-        puts "#{allergies} has been added. Type 'done' when finished"
-      end
+  puts "Please type out your allergies. If none or you have finished please type done"
+  allergies = gets.chomp.downcase
+
+        if allergies == "sunshine"
+            vampire_status = "probabaly a vampire"
+          elsif allergies == "done"
+            puts "Thanks for your submission"
+          else
+            puts "#{allergies} has been added. Type 'done' when finished"
+        end
+
   end
 
-number_employees -= 1
-	puts "Test Results for #{name} is #{vampire_status}"
-    puts "Ready for next employee."
+  number_employees -= 1
+
+  puts "Test Results for #{name} is #{vampire_status}"
+  puts "Ready for next employee."
 
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends. Let's set up a committee to discuss werewolf & vampire integration programs."
