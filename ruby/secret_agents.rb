d3bd45161f6cc464f360eba04b549abe44695d8c
Amanda Lin <adnama.lin@gmail.com>
2016-04-14 19:39:30 -0500

    Start 4.6 peer pair

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,11 @@
+
+
+#BEGIN
+#Encrypt Method
+#Ask user what password they would like to encrypt
+#Define password to encrypt
+#Identify index numbers for letters in password
+#Any space character remains a space character after encryption
+#Shift letters by one to encrypt
+#Print encrypted password
+#END
