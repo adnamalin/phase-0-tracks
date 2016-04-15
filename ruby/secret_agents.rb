6a3bfcc9e880de6752a1747c9a8b9b854f4ac073
Amanda Lin <adnama.lin@gmail.com>
2016-04-14 19:56:31 -0500

    Work on release 2 encryption

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,3 +9,31 @@
 #Shift letters by one to encrypt
 #Print encrypted password
 #END
+
+puts "What do you want to encrypt?"
+enc_password = gets.chomp
+
+def encrypt(input)
+  index = 0
+    while index < input.length
+      input[index] = input[index].next!
+      index += 1
+    end
+    p input
+end
+
+encrypt(enc_password)
+
+
+
+#BEGIN
+# Decrypt Method
+#Ask user what password they would like to decrypt
+#Define password to decrypt
+#Identify index numbers for letters in password
+#Any space character remains a space character after encryption
+#Call index on each letter of the password
+#Use index number found to compare with "abcdefghijklmnopqrstuvwxyz"
+#Call index number -1
+#Print decrypted password
+#END
