dadd0b33b066dd0fa7b425abbd77bde778f07e75
MikeyMoore <icarusjedi@gmail.com>
2016-04-15 18:50:08 -0600

    save driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -59,3 +59,22 @@ p decrypt(encrypt("swordfish"))
 # This works because the decrypt method will run the output of the encrypt method that is nested inbetween. The encrypt output is "txpsegjti" so the final run of this line is decrypt("txpsegjti") which returns "swordfish"
 
 #DRIVER CODE
+
+decision = ""
+until decision == "encrypted" || decision == "decrypted"
+#Ask user if they want to encrypt or decrypt
+puts "Would you like your password encrypted or decrypted?"
+decision = gets.chomp.downcase
+end
+#Ask them for password
+puts "What is your password?"
+psswrd = gets.chomp
+#Decide which method
+#Run method
+#Print results
+if decision == "encrypted" 
+  p encrypt(psswrd)
+else decision == "decrypted"
+  p decrypt(psswrd)
+end
+
