5aca3e06cfe97a8beb962b9ac70a327ba8feb338
Amanda Lin <adnama.lin@gmail.com>
2016-04-17 23:17:27 -0500

    Add .ord number explanation

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,37 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# p "iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# p “zom”.insert(1, "o")
+p “zom”.insert(1, "o")
 # => “zoom”
 
-# p "enhance".center(20)
+p "enhance".center(20)
 #=> “    enhance    ”
 
-# p "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# p "the usual".concat(" suspects")
+p "the usual".concat(" suspects")
 # "the usual" << " suspects"
 #=> “the usual suspects”
 
-# p "suspects".prepend("the usual ")
+p "suspects".prepend("the usual ")
 #=> “the usual suspects”
 
-# p "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => “The case of the disappearing last lette”
 
-# p "The mystery of the missing first letter"[1..-1]
+p "The mystery of the missing first letter"[1..-1]
 # => “he mystery of the missing first letter”
 
-# p "Elementary  ,    my   dear      Watson!".squeeze(" ")
+p "Elementary  ,    my   dear      Watson!".squeeze(" ")
 # => “Elementary, my dear Watson!”
 
-# p "z".ord
+p "z".ord
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
+#Returns the codepoint for correlating letter on the UTF-8 single-byte character encoding. 
 
-# p "How many times does the letter ‘a’ appear in this string?".count("a")
+p "How many times does the letter ‘a’ appear in this string?".count("a")
 #=> 4
