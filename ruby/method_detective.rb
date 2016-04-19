be17e726033f2d94f1f76fff1b5e01b990d1795a
Andrea Frank <franklette70@gmail.com>
2016-04-18 20:49:09 -0500

    complete 3 last methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -17,23 +17,23 @@
  => "STOP! YOU’RE UNDER ARREST!"
 
  "the usual" << " suspects"
-=> "the usual suspects"
+ => "the usual suspects"
 
  " suspects".prepend("the usual")
  => "the usual suspects"
 
-"The case of the disappearing last letter".chop
-# => "The case of the disappearing last lette"
+ "The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
  "The mystery of the missing first letter".slice(1..-1)
-# => "he mystery of the missing first letter"
+ => "he mystery of the missing first letter"
+ 
+ "Elementary,    my   dear        Watson!".squeeze(" ")
+ => "Elementary, my dear Watson!"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+ "z".each_byte {|c| print c, ' ' }
+  => 122 
+  (What is the significance of the number 122 in relation to the character z?)
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
-
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+ "How many times does the letter 'a' appear in this string?".count("a")
+ => 4
