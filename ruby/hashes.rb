8c3e6f9e462d865c02b2d0fb921661ef24fb84f2
Amanda Lin <adnama.lin@gmail.com>
2016-04-20 14:50:56 -0500

    Working version using methods

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,4 @@
+#BEGIN ------
 #Prompt designer for inputs
 #Make sure inputs convert to proper data type
 #Inputs will go into the hash
@@ -7,7 +8,9 @@
 #User enters key they want to change the value of
 # kay & value are updated in the hash
 #Print the final version of hash
+# ------ END
 
+#Method to gather client data
 def client_profile()
   puts "What is the clents full name?"
   full_name = gets.chomp
@@ -32,28 +35,64 @@ def client_profile()
       client_floor = false
     end
 
-  client_details = {name: full_name, :age => client_age, :children => client_kids, :decortheme => client_theme, :rooms => client_roompref, :hardwood => client_floor}
+  client_details = {
+    :name => full_name,
+    :age => client_age,
+    :children => client_kids,
+    :decortheme => client_theme,
+    :rooms => client_roompref,
+    :hardwood => client_floor}
+
   return client_details
 end
 
+#Method to correct client data
 def corrections()
   puts "What key would you like to correct?"
   key_to_correct = gets.chomp.to_sym
 
   puts "What is the new value for this key?"
-  new_value = gets.chomp
-
+    #Case statement to adjust data types
+    case key_to_correct
+      when :age
+        new_value = gets.chomp.to_i
+      when :children
+        new_value = gets.chomp.to_i
+      when :rooms
+        new_value = gets.chomp.to_i
+      when :hardwood
+        new_value = gets.chomp
+          if new_value== "yes"
+            new_value = true
+          else
+            new_value= false
+          end
+      else
+        new_value = gets.chomp
+    end
+    #Turning correction into an array
+  corrections_array =[key_to_correct, new_value]
+  return corrections_array
 end
 
+########### DRIVER CODE ##########
 
 #Ask designer for client information
-p client_profile()
+client_info = client_profile()
+p client_info
 
 #Give user chance to correct a key
-# puts "Do you need to correct a key, yes or no?"
-# need_correction = gets.chomp
-#   if need_correction == "yes"
-#     corrections()
-#   else
-#     puts "Great!"
-#   end
+puts "Do you need to correct a key, yes or no?"
+needs_correction = gets.chomp
+#Run appropriate response and prints final results
+  if needs_correction == "yes"
+    make_corrections = corrections()
+    #Using array make_corrections to push new key & value into existing hash 
+    client_info[make_corrections[0]] = make_corrections[1]
+    puts "Great! Here is the final version for this client's profile: #{client_info}"
+  else
+    puts "Great! Here is the final version for this client's profile: #{client_info}"
+  end
+
+#Thank user
+puts "Thanks for using our program!"
