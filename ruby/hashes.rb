7a756ae8d4c1b8a6bc5c352146306f6d0a7e67f3
Andrea Frank <franklette70@gmail.com>
2016-04-20 19:39:30 -0500

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+# PSEUDOCODE: 
+# Ask designer for multiple pieces of information about a client. 
+# Store responses.
+# Display responses in a hash.
+# Display a question asking if any of the items need to be changed.
+# Give option for none.
+# If something does need to be changed, ask for updated value.
+# Replace original value with new value.
+# Print final hash.
