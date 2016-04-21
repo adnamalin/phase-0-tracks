4340b8b8fb31bbc8c8002040a2f4a31e812c8336
Amanda Lin <adnama.lin@gmail.com>
2016-04-21 13:40:40 -0500

    Add nested structure for Netflix library

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,21 @@
+netflix_library = {
+  'tv_shows': {
+    'total_shows': 4
+    'tv_comedy': ['Unbreakable Kimmy Schimidt', 'Parks and Recreation']
+    'tv_drama': ['House of Cards', 'Black Mirror']
+  }
+  'documentaries': {
+    'total_movies': 5
+    'critically_acclaimed': ['Jiro Dreams of Sushi', 'Somn', 'For Grace']
+    'foreign': ['The Act of Killing', 'The Kingdom of Dreams and Madness']
+  }
+  'comedies': {
+    'total_movies': 4
+    'romantic_comedy': ['Princess Bride', 'Man Up', 'Along Came Polly']
+    'black_comedy': ['God Bless America']
+  }
+  'dramas': {
+    'total_movies': 4
+    'war': ['Fury', 'Inglorious Basterds','Red Cliff', 'Black Hawk Down']
+  }
+}
