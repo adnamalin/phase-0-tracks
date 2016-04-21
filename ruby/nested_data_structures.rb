701373edbf2f79e6a054906c0850a154c70a5107
Andrea Frank <franklette70@gmail.com>
2016-04-20 20:55:44 -0500

    add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+grocery_store = {
+    produce: {
+        location: "front of store",
+        apples: [
+            "gala",
+            "braeburn",
+            "fuji"
+        ],
+        potatoes: [
+			"sweet",
+			"russet",
+			"fingerling"
+		]	
+    },
+    meats: {
+        location: "butcher counter",
+        pork: [
+            "belly",
+            "bacon",
+            
+        ],
+        beef: [
+            "tri-tip",
+            "sirloin",
+            "rib-eye"
+        ],
+        chicken: [
+        	"breast"	
+        ]
+    },
+    dairy: {
+        location: "refrigerators", 
+        dairy_types: {
+            milk:  [
+            	"whole",
+            	"skim",
+            	"2%",
+            	"soy"
+            ],
+            yogurt: "greek",
+            cheese: [
+            	"mozzarella",
+            	"parmesan"
+            ]
+        },
+    }
+}
+
+p grocery_store
