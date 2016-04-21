dbb1cf5bfd33e388f8f02e8743ba98e0e026d287
Andrea Frank <franklette70@gmail.com>
2016-04-20 19:40:49 -0500

    create hash and add list of questions for designer to answer

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,3 +7,28 @@
 # If something does need to be changed, ask for updated value.
 # Replace original value with new value.
 # Print final hash.
+
+# Create empty hash
+ client = {}
+
+# Obtain multiple pieces of information from the designer about the client 
+ puts "What is the client's name?"
+ client[:name] = gets.chomp.capitalize
+
+ puts "Please enter the client's address:"
+ client[:addr] = gets.chomp
+
+ puts "Please enter the client's age:"
+ client[:age] = gets.chomp.to_i
+
+ puts "How many children does the client have?"
+ client[:children] = gets.chomp.to_i
+
+ puts "What is your decor theme?"
+ client[:decor] = gets.chomp
+
+ puts "Are you willing to have a interior design consultation? (y/n)"
+ client[:consult] = gets.chomp
+
+# Print hash
+p client
