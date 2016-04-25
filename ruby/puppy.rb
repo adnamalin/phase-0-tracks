d27eb49e79d7baa48259be173d8fb30ae222e74b
Amanda Lin <adnama.lin@gmail.com>
2016-04-25 14:04:26 -0500

    Add puppy behaviors and characteristics for 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------
+# Puppitius Cuddulium
+
+# CHARACTERISTICS ----------------
+#legs: 4
+#hair color: varies
+#size: small
+#adorableness: high
+#friendliness: high
+#self-control: low
+#name: varies
+
+# BEHAVIOR -----------------------
+# Bark
+# Sit
+# Play Dead
+# Puppy Eyes
