f90bc82549f9a5acf4c63d9d2852b0c567cb78dc
Andrea Frank <franklette70@gmail.com>
2016-04-25 21:50:35 -0500

    add puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ---------------------
+Puppy
+
+CHARACTERISTICS -------------
+color: varies
+bark_dB: loud
+paws: 4
+tail: 1
+furry: yes
+
+
+BEHAVIOR --------------------
+Bark 
+Nip
+Sleep
+Eat
