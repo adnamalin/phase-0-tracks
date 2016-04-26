ea9674396a2de8de925dd133615048564030e1af
Amanda Lin <adnama.lin@gmail.com>
2016-04-26 17:44:14 -0500

    Create Santa Class

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santas
+
+  def initialize
+    puts "Initializing Santa Instance.."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookes(cookiename)
+    puts "That was a good #{cookiename}!"
+  end
+  
+end
