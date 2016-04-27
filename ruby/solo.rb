8d0457b6f8fef34ac9cc6a7b33b811470e990b59
Amanda Lin <adnama.lin@gmail.com>
2016-04-26 21:56:50 -0500

    Add class methods and attributes

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,16 +1,46 @@
 ####BEGIN####
   #Class: Pikachu
-
   #Attributes:
     #Name
     #Type
-    #Hometown
+    #Hometown:Kanto, Johto, Hoenn, Sinnoh, Kalos
     #Attacks: Thunder shock, Growl, Wild Charge
-    #Human_Age (pokemon age * 9)
-
-  #Methods: 
+    #Age
+  #Methods:
     #Attack: randomizes attacks and prints one
     #Speak: prints "pika pika!"
     #Age Convert: takes age integer and * 9
-
 #####END#####
+
+class Pikachu
+  attr_reader :type, :attacks
+  attr_accessor :name, :hometown, :age
+
+  def initialize(name, hometown, age)
+    @name = name
+    @hometown = hometown
+    @age = age
+    @type = "electric"
+    @attacks = ["Thundershock", "Growl", "Wild Charge"]
+  end
+
+  def attack
+    p " #{name} uses: #{@attack.sample}"
+  end
+
+  def speak
+    puts "PIKA PIKA!"
+  end
+
+  def age_convert(poke_age)
+    human_age = poke_age * 9
+  end
+
+end
+
+# pika = Pikachu.new("fred", "Johto", 10)
+# p pika.name
+# p pika.type
+# p pika.attacks
+# p pika.hometown
+# p pika.age
