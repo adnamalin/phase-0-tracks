fbc191a843fe86758c1e5c48c7fa556bac4aa759
Amanda Lin <adnama.lin@gmail.com>
2016-04-26 21:46:16 -0500

    Pseudocode class pikachu

---
+++ ruby/solo.rb
@@ -0,0 +1,16 @@
+####BEGIN####
+  #Class: Pikachu
+
+  #Attributes:
+    #Name
+    #Type
+    #Hometown
+    #Attacks: Thunder shock, Growl, Wild Charge
+    #Human_Age (pokemon age * 9)
+
+  #Methods: 
+    #Attack: randomizes attacks and prints one
+    #Speak: prints "pika pika!"
+    #Age Convert: takes age integer and * 9
+
+#####END#####
