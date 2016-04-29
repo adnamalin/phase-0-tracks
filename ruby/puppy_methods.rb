2862543ab9294edac334d8e9beb13cea3b9d318e
Andrea Frank <franklette70@gmail.com>
2016-04-28 21:15:21 -0500

    complete release 1, adding methods wihtin class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,38 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
 
-end
+	def speak(integer)
+		5.times do 
+			puts "Bork"
+		end
+	end
+  	
+ 	def roll_over
+ 		puts "*rolls over*"
+ 	end
 
+	def dog_years(age)
+		puts "How old are you in human years?"
+			years.to_i = gets.chomp
+			age = years/7
+			return age
+	end
+
+	def shake
+		
+
+
+
+ end
+
+# DRIVER CODE #
+
+little_dog = Puppy.new
+little_dog.fetch("kong")
+little_dog.speak(4)
+little_dog.roll_over
+little_dog.dog_years(7)
