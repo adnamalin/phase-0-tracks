6a474a3365b269c223b4220a0329e840e8d033e8
Andrea Frank <franklette70@gmail.com>
2016-04-29 23:07:03 -0500

    complete release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+#create class
 class Santa
   def initialize(name, suit_color)
     @name = name
@@ -37,20 +38,68 @@ class Santa
 # p hairy_face("no")
     
   def about(name)
-    puts "Hi, my name is #{@name} and I identify as #{@gender}. I am #{@ethnicity} and I am #{@age} years old."
+    puts "Hi, my name is #{@name} and I identify as #{@gender}. I am #{@ethnicity} and I am #{@age} years old. My reindeer ranks are as follows: #{@reindeer_ranking}."
+  
   end
+
+  def celebrate_birthday
+    @age += 1
+  end
+  
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << (reindeer)
+  end
+ 
+  # gender getter method
+  def gender
+    @gender
+  end
+  
+  # gender setter method
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+  
+  # age getter method
+   def age
+      @age
+   end  
+  
+  # age setter method
+   def age=(new_age)
+      @age = new_age
+   end
+  
+   # ethnicity getter method
+   def ethnicity
+      @ethnicity
+   end  
+  
+  # ethnicity setter method
+   def ethnicity=(new_ethnicity)
+      @ethnicity = new_ethnicity
+   end
+  
+  
 end
 
 # DRIVER CODE #
 
 santa = Santa.new("Julius", "orange")
-santa.genders("a cool dudette")
-santa.ethnicities("Latina")
-santa.hairy_face("no")
-# santa.age
+# santa.genders("a cool dudette")
+ santa.ethnicities("Latina")
+# santa.hairy_face("no")
+santa.age
 # santa.about
-# santa.reindeer_ranking
-# santa.about
-santa.speak
-santa.eat_milk_and_cookies("gingersnap")
-santa.about("Julius")
+# santa.speak
+# santa.eat_milk_and_cookies("gingersnap")
+# santa.about("Julius")
+
+p santa.get_mad_at("Vixen")
+santa.gender = "superfemme"
+puts "Now I'm #{santa.gender}!"
+
+p santa.age
+p santa.ethnicity
+
