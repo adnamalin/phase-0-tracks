77671fbb3e4e88389bafcfed4458a513ec3f8c34
Andrea Frank <franklette70@gmail.com>
2016-04-30 17:16:10 -0500

    complete release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,14 @@
-# Age isn't adding +1 year because method isn't called?
-
 #create class
 class Santa
   attr_accessor :gender, :age, :ethnicity
   
-  def initialize(name, suit_color)
-    @name = name
-    @gender = "lady"
-    @ethnicity = "aboriginal"
-    @suit_color = suit_color
-    @facial_hair = "no"
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  def initialize(gender, ethnicity)
+#    @name = name
+    @gender = gender
+    @ethnicity = ethnicity
+#    @suit_color = suit_color
+#    @facial_hair = "no"
+#    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
   
@@ -54,25 +52,40 @@ class Santa
     @reindeer_ranking.delete(reindeer)
     @reindeer_ranking << (reindeer)
   end
-  
+ 
+#method for release 4  
+def santa_individuals
+   puts "-------------------------------"
+   puts "Here is one santa in the group!"
+   puts "Gender: #{@gender}"
+   puts "Ethnicity: #{@ethnicity}"
 end
 
-# DRIVER CODE #
+end
 
-santa = Santa.new("Julius", "orange")
+# RELEASE 4
+santas_gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+santas_ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+ 
+100.times {
+Santa.new(santas_gender.sample, santas_ethnicity.sample).santa_individuals}
+
+
+# RELEASE 0-3 DRIVER CODE #
+# santa = Santa.new("Julius", "orange")
 # santa.genders("a cool dudette")
- santa.ethnicities("Latina")
+# santa.ethnicities("Latina")
 # santa.hairy_face("no")
-santa.age
+# santa.age
 # santa.about
 # santa.speak
 # santa.eat_milk_and_cookies("gingersnap")
 # santa.about("Julius")
 
-p santa.get_mad_at("Vixen")
-santa.gender = "superfemme"
-puts "Now I'm #{santa.gender}!"
+# p santa.get_mad_at("Vixen")
+#santa.gender = "superfemme"
+# puts "Now I'm #{santa.gender}!"
 
-p santa.age
-p santa.ethnicity
-
+# p santa.age
+# p santa.ethnicity
