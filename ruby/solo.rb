5c72371d0bb94066e65d7ce41e736d4373805f63
Andrea Frank <franklette70@gmail.com>
2016-04-30 17:53:41 -0500

    comadd pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+# PSEUDOCODE
+#
+# SPACESHIP class
+#
+# Three attributes: 
+# size-string
+# amount of fuel needed-integer
+# whether it is currently in orbit-boolean
+# 
+# Three methods (at least one needs to take an argument): 
+# land
+# takeoff
+# fly in space at some rate - (this will take a boolean variable.)
+
