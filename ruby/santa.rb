702fd2f2a99ae0d8373b67a34f3400a3ae81ff1c
Andrea Frank <franklette70@gmail.com>
2016-04-29 21:07:46 -0500

    correct release 1, basically re-writing the whole thing

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,39 +1,56 @@
-# Your class should have three instance methods:
-
-# A speak method that will print "Ho, ho, ho! Haaaappy 
-# holidays!" (Santa has been working on being more inclusive.)
-# An eat_milk_and_cookies method that takes a cookie type 
-#(example: "snickerdoodle") as a parameter and prints "That was a good 
-#<type of cookie here>!" 
-# An initialize method that prints "Initializing Santa instance ...".
-# Add a bit of code below your class declaration to check that you're 
-#able to initialize a Santa instance and call its methods.
-
 class Santa
+  def initialize(name, suit_color)
+    @name = name
+    @gender = "lady"
+    @ethnicity = "aboriginal"
+    @suit_color = suit_color
+    @facial_hair = "no"
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
 
-	def initialize(gender)
-		@gender = gender
-		@ethnicity = ethnicity
-		@suit_color = suit_color
-		@facial_hair = facial_hair
-		countrycount.times = { |country_count| puts "Santa has visited this country #{country_count} times this year." }
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
-	end
+  def speak
+    puts "Ho, ho, ho! Haaaaappy Holidays!"
+  end
 
-	def speak
-		puts "Ho, ho, ho! Haaaaappy Holidays!"
-	end
+  def eat_milk_and_cookies(type_of_yummy_snack)
+    puts "That was a good #{type_of_yummy_snack}!"
+  end
 
-	def eat_milk_and_cookies(type_of_yummy_snack)
-		puts "That was a good #{type_of_yummy_snack}!"
-	end
+  def genders(gender)
+    puts "I identify as #{gender}."
+    @gender = gender
+  end
+  
+  def ethnicities(ethnicity)
+    puts "My ethnicity is #{ethnicity}."
+    @ethnicity = ethnicity
+  end
+  
+  def hairy_face(facial_hair)
+    if facial_hair == "yes"
+      puts "Scratchy!"
+    else
+      puts "Soft!"
+    end
+  end 
+# p hairy_face("no")
+    
+  def about(name)
+    puts "Hi, my name is #{@name} and I identify as #{@gender}. I am #{@ethnicity} and I am #{@age} years old."
+  end
 end
 
 # DRIVER CODE #
 
-Klaus = Santa.new
-
-Klaus.speak
-Klaus.eat_milk_and_cookies("gingersnap")
-Klaus.initialize()
+santa = Santa.new("Julius", "orange")
+santa.genders("a cool dudette")
+santa.ethnicities("Latina")
+santa.hairy_face("no")
+# santa.age
+# santa.about
+# santa.reindeer_ranking
+# santa.about
+santa.speak
+santa.eat_milk_and_cookies("gingersnap")
+santa.about("Julius")
