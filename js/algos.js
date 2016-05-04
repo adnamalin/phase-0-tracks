8b4a56a6f63c6f1b339778f91a3b96180a65b6d8
Amanda Lin <adnama.lin@gmail.com>
2016-05-04 12:56:46 -0500

    Finish search function for 7.3

--- js/algos.js
+++ js/algos.js
@@ -19,30 +19,41 @@ function findLongest(list){
 
 /////////////////////// RELEASE 1
 //Take two objects
-//Iterate through one objects key/pair value and compare to other object
-//Returns true if there is a match
+//Store matchFound as false to start
+//Iterate through one objects key/pair value store
+//Iterate through other object and set up conditionals to compare
+//If conditional is met and a match is found then matchFound will be updated to true if not, it does not change
+//Return matchFound results
 
-var me = {name: "Amanda", age: 23, ethnicity: "Chinese"}
-var other = {name: "Bob", age: 23, ethnicity: "Japanese"}
-
-for (var key in me) {
-  compareKey = key;
-  compareProp = me[key];
-    for (otherkey in other){
-      if (compareKey == otherkey && compareProp == other[otherkey]){
-       console.log(compareKey + "=" + otherkey + " &" + compareProp + "=" + other[otherkey] +" true")
-       } else {
-        console.log(compareKey + " !=" + otherkey + " &" + compareProp + "!=" + other[otherkey] + " false")
-       }
-    }
+function search(obj1, obj2){
+  var matchFound = false;
+  for (var key in obj1) {
+    compareKey = key + " =" + obj1[key];
+      for (key in obj2){
+        otherKey = key + " =" + obj2[key];
+        if (compareKey == otherKey){
+          // console.log(compareKey + " =" + otherKey + " true") //Prints out each comparison
+          matchFound = true
+         } else {
+          //  console.log(compareKey + "!=" + otherKey + " false") //Prints out each comparison
+         }
+      }
+  }
+  console.log(matchFound)
 }
 
-
-
-
-
-
 /////////////////////// Driver Code
-// findLongest(['hi', 'hey','wazzzzzzzzup', 'hello', 'salutations', 'sup'])
-// findLongest(['xxxxxxxxxxxxx', 'x','xx', 'xxx', 'xxx', 'xxxx'])
-// findLongest(['blah', 'blahhher','blahhhhhhhest'])
+//findLongest Driver Code
+  findLongest(['hi', 'hey','wazzzzzzzzup', 'hello', 'salutations', 'sup'])
+  findLongest(['xxxxxxxxxxxxx', 'x','xx', 'xxx', 'xxx', 'xxxx'])
+  findLongest(['blah', 'blahhher','blahhhhhhhest'])
+
+// Search Driver Code
+  console.log("Should be true:")
+   search({name: "Amanda", age: 23, ethnicity: "Chinese"},{age: 23, name: "Bob", ethnicity: "Japanese"})
+  console.log("Should be false:")
+    search({dog: "fido", cat: "marigold", hippo: "bob"},
+    {cat: "fido", dog: "marigold", giraffe: "bob"})
+  console.log("Should be true:")
+    search({wed: "sunny", mon: "rainy", fri: "hurricane"},
+    {fri: "hurricane", tue: "snow", mon: "thunder"})
