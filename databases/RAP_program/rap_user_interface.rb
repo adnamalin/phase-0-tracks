357444385866d8be51e128afe9c3d1fbe4a99388
Amanda Lin <adnama.lin@gmail.com>
2016-05-13 13:28:07 -0400

    Add basic pseudocode for user interface

--- databases/RAP_program/rap_user_interface.rb
+++ databases/RAP_program/rap_user_interface.rb
@@ -2,6 +2,11 @@ require_relative 'rap_business_logic'
 require 'sqlite3'
 #Trying use two different files to run this program. I want to figure out if I can access roomate_db across files without making it a global variable.
 
+#Ask user if they want to work with contact info, chores, or bills.
+  #Contact info: add roomate, update roomate, or view roomates
+  #Chores: view chores, or update who did it Last
+  #Bills: view bills or add new bill value
+
 #TESTING ADD ROOMATE
   # puts "Add roommate name:"
   # rm_name = gets.chomp
