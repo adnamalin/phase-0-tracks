abfaf2e65a34ecb39859876c1123e8f1c5fc4cad
Amanda Lin <adnama.lin@gmail.com>
2016-05-18 15:49:35 -0500

    Working version for initial JQuery edits

--- web_dev/jquery/amandajquery.js
+++ web_dev/jquery/amandajquery.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+
 //About Page
     $( "button" ).click(function() {
-        $( "#FavTable" ).slideToggle( "slow" );
+        $("#FavTable").slideToggle("slow");
         $("FavTable").css("display","block");
+    });
+    
+    $("#img1").click(function() {
+       $(this).attr("src", "images/sillyimg1.png");
     });
     
 });
