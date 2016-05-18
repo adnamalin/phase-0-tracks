188ddfa4b4b283db2af5a2194c06eda972c90487
Amanda Lin <adnama.lin@gmail.com>
2016-05-18 16:54:10 -0500

    Switch images on hover

--- web_dev/jquery/amandajquery.js
+++ web_dev/jquery/amandajquery.js
@@ -6,8 +6,22 @@ $(document).ready(function(){
         $("FavTable").css("display","block");
     });
     
-    $("#img1").click(function() {
-       $(this).attr("src", "images/sillyimg1.png");
+    $("#img1").hover(function() {
+       $(this).attr("src", "images/sillyimage1.png");
+    }, function() {
+        $(this).attr("src", "images/AmandaLin-Cropped2.jpg");
+    });
+    
+    $("#img2").hover(function() {
+       $(this).attr("src", "images/sillyimage2.png");
+    }, function() {
+        $(this).attr("src", "images/AmandaLin-Cropped.png");
+    });
+    
+     $("#img3").hover(function() {
+       $(this).attr("src", "images/sillyimage3.png");
+    }, function() {
+        $(this).attr("src", "images/AmandaLin-Cropped3.jpg");
     });
     
 });
